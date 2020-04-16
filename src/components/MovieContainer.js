35ac6a069b50bd30cc6779039ed3b99a50073d6d
Joe Dodd <jdodd@tccm.org>
2020-04-16 13:40:51 -0400

    Further UI/UX refinements

--- src/components/MovieContainer.js
+++ src/components/MovieContainer.js
@@ -2,11 +2,14 @@ import React from "react";
 import "./Movie.css";
 import axios from "axios";
 
+const convertDate = (d) => new Date(d).toDateString();
+
 export default class MovieContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       movie: {},
+      movieCredits: []
     };
   }
   componentDidMount() {
@@ -15,12 +18,16 @@ export default class MovieContainer extends React.Component {
       .get(
         `${process.env.REACT_APP_MOVIE_API_URL}/movies/movie?movieId=${movieId}`
       )
-      .then((res) => this.setState({ movie: res.data.data }))
+      .then((res) => {
+        this.setState({
+          movie: { ...res.data.movie },
+          movieCredits: [...res.data.movieCredits],
+        });
+      })
       .catch((err) => this.setState({ error: err }));
   }
   render() {
-    console.log(this.state);
-    const { movie } = this.state;
+    const { movie, movieCredits } = this.state;
     return (
       <>
         <div
@@ -29,7 +36,7 @@ export default class MovieContainer extends React.Component {
             backgroundImage: `linear-gradient(rgba(255, 255, 255, 0), rgb(17, 17, 17) 100%), url(http://image.tmdb.org/t/p/original${movie.poster_path})`,
           }}
         >
-          <div class="hero-info-container">
+          <div className="hero-info-container">
             <img
               src={`http://image.tmdb.org/t/p/w342${movie.poster_path}`}
               alt="Movie Poaster"
@@ -40,7 +47,33 @@ export default class MovieContainer extends React.Component {
         </div>
         <div className="movie-info-wrapper">
           <div className="movie-info-container">
-            <p>{movie.overview}</p>
+            <div className="movie-info-item">
+              <h3>Release Date</h3>
+              <span>
+                {!!movie.release_date && convertDate(movie.release_date)}
+              </span>
+            </div>
+            <div className="movie-info-item">
+              <h3>Genres</h3>
+              {!!movie.genres &&
+                movie.genres.map((g) => <span>{g.name}&nbsp;</span>)}{" "}
+            </div>
+            <div className="movie-info-item">
+              <h3>Synopsis</h3>
+              <p>{movie.overview}</p>
+            </div>
+            <div className="movie-info-item">
+              <h3>Cast</h3>
+              {!!movieCredits && (
+                <div className="movie-cast-container">
+                  {movieCredits.map((c) => (
+                    <span key={c.name}>
+                      <strong>{c.name}:</strong> {c.character}
+                    </span>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </>
--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -17,25 +17,26 @@ const MoviesList = ({ movies, handleClick }) => {
       className="movies-list-container"
       style={{ gridTemplateRows: `repeat(${movies.length}, 1fr)` }}
     >
-      {movies.length &&
+      {!!movies &&
+        !!movies.length &&
         movies.map((m) => (
-          <MovieListItem movie={m} handleClick={handleClick} />
+          <MovieListItem movie={m} handleClick={handleClick} key={m.id}/>
         ))}
     </div>
   );
 };
 
-const MovieSearch = ({ queryValue, onChange, onClick }) => {
+const MovieSearch = ({ queryValue, onChange, onSubmit }) => {
   return (
-    <>
+    <form onSubmit={onSubmit}>
       <input
         type="text"
         name="movieQuery"
         value={queryValue}
         onChange={onChange}
       />
-      <button onClick={onClick}>Search</button>
-    </>
+      <input type="submit" value="Search" />
+    </form>
   );
 };
 
@@ -54,16 +55,17 @@ export default class MoviesContainer extends React.Component {
   componentDidMount() {
     axios
       .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`)
-      .then((res) => this.setState({ movies: res.data.data }))
+      .then((res) => this.setState({ movies: res.data.movies }))
       .catch((err) => this.setState({ error: err }));
   }
 
   handleChange = (e) => {
-    e.persist();
     this.setState({ queryValue: e.target.value });
   };
 
-  handleQuery = () => {
+  handleQuery = (e) => {
+    e.preventDefault();
+    e.persist();
     const { queryValue } = this.state;
     if (!queryValue) {
       return this.setState({
@@ -76,7 +78,7 @@ export default class MoviesContainer extends React.Component {
         `${process.env.REACT_APP_MOVIE_API_URL}/movies/search?search=${queryValue}`
       )
       .then((res) =>
-        this.setState({ movies: res.data.data, queryError: false })
+        this.setState({ movies: res.data.movies, queryError: false })
       )
       .catch((err) =>
         this.setState({
@@ -100,7 +102,7 @@ export default class MoviesContainer extends React.Component {
             {queryError && <span>{errorMessage}</span>}
             <MovieSearch
               onChange={this.handleChange}
-              onClick={this.handleQuery}
+              onSubmit={this.handleQuery}
               queryValue={queryValue}
             />
           </div>
